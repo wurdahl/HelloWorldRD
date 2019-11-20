54487fcf78795c278174aa935d5bee02c63dcb90
wurdahl <wurdahl@gmail.com>
2019-11-20 15:27:49 -0600

    Hacking lol

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
 
+
+    public static void main(String Args[]){
+        System.out.println("LMAO You got hacked");
+    }
+
 	//this code was garbage
 	//this is added from a different Branch
 }
